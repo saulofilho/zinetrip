acf77d4249ca1eb3da04c255227a84ccea543966
saulo filho <hello@saulofilho.com>
2021-09-14 18:35:09 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         background_color: `#ff33cc`,
         theme_color: `#ff33cc`,
         display: `minimal-ui`,
-        icon: `static/assets/Trip---paginas.png`,
+        icon: `https://zinetrip.site/assets/Trip---paginas.png`,
       }
     },
     'gatsby-plugin-offline',
